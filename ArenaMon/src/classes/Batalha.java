2f5249a28f4b083d02c1c90acfec93833756aa8d
Lauramatos29 <lauramatos051@gmail.com>
2025-01-21 10:47:44 -0300

    Update Batalha.java

--- ArenaMon/src/classes/Batalha.java
+++ ArenaMon/src/classes/Batalha.java
@@ -16,6 +16,7 @@ public class Batalha extends Pokemon {
 	            System.out.println("2 - Ataque 2");
 	            System.out.println("3 - Ataque 3");
 	            System.out.println("4 - Ataque 4");
+	            
 	            while (!scanner.hasNextInt()) {
 	                System.out.println("Entrada inválida. Digite um número entre 1 e 4.");
 	                scanner.next(); 
@@ -58,20 +59,116 @@ public class Batalha extends Pokemon {
 	                System.out.println(nome + " atacou " + oponente.nome + " causando " + dano + " de dano \n");
 	                
 	                break;
+	                
+	                default:
+	                	System.out.println("Opção inválida! Tente de novo.");
 	        }
+	    }
+	    
+	    public Pokemon trocarPokemon(Scanner scanner) {
+	    	System.out.println("Escolha o seu novo Pokémon:");
+	        System.out.println("1 - Pikachu");
+	        System.out.println("2 - Charmander");
+	        System.out.println("3 - Bulbasaur");
+	        System.out.println("4 - Squirtle");
+	        
+	        int choosePokemon;
+	        Pokemon novoPokemon = null;
+	        
+	        while(novoPokemon == null) {
+	        	while(!scanner.hasNextInt()) {
+	        		System.out.println("Entrada inválida! Digite um número entre 1 e 4");
+	        		scanner.next();
+	        	}
+	        	
+	        	choosePokemon = scanner.nextInt();
+	        	
+	        	switch(choosePokemon) {
+	        	
+	        	case 1:
+	        		novoPokemon = new Batalha("Pikachu", "Elétrico", 100, 50, 30);
+	        		break;
+	        		
+	        	case 2:
+	        		novoPokemon = new Batalha("Charmander", "Fogo", 90, 60, 25);
+	        		break;
+	        		
+	        	case 3:
+	        		novoPokemon = new Batalha("Bulbasaur", "Planta", 95, 55, 35);
+	        		break;
+	        		
+	        	case 4:
+	        		novoPokemon = new Batalha("Squirtle", "Água", 100, 45, 40);
+	        		break;
+	        		
+	        		default:
+	        			System.out.println("Erro! Opção inválida.");
+	        	}
+	        }
+			return novoPokemon;
+	        
+
 	    }
 	    
 	    public static void main(String[] args) {
 	        Scanner scanner = new Scanner(System.in);
-	        Pokemon jogador = new Batalha("Picachu", "Fogo", 100, 50, 30);
-	        Pokemon oponente = new Batalha ("Chamander", "Fogo", 90, 60, 25);
-
+	        
+	        Pokemon pikachu = new Batalha("Pikachu", "Elétrico", 100, 50, 30);
+	        Pokemon charmander = new Batalha("Charmander", "Fogo", 90, 60, 25);
+	        Pokemon bulbasaur = new Batalha("Bulbasaur", "Planta", 95, 55, 35);
+	        Pokemon squirtle = new Batalha("Squirtle", "Água", 100, 45, 40);
+	        
+	        System.out.println("ArenaMon - Batalha de Pokemons");
+	        System.out.println("Escolha o seu Pokemon:");
+	        System.out.println("1 - Pikachu");
+	        System.out.println("2 - Charmander");
+	        System.out.println("3 - Bulbasaur");
+	        System.out.println("4 - Squirtle");
+	        
+	        int escolhaPokemon;
+	        Pokemon jogador = null;
+	        
+	        while (jogador == null) {
+	        	while(!scanner.hasNextInt()) {
+	        		System.out.println("Entrada inválida. Digite um número entre 1 e 4.");
+	        		scanner.next();
+	        	}
+	        	
+	        	escolhaPokemon = scanner.nextInt();
+	        	
+	        	switch(escolhaPokemon) {
+	        	
+	        	case 1:
+	        		jogador = pikachu;
+	        		break;
+	        		
+	        	case 2:
+	        		jogador = charmander;
+	        		break;
+	        		
+	        	case 3:
+	        		jogador = bulbasaur;
+	        		break;
+	        		
+	        	case 4:
+	        		jogador = squirtle;
+	        		break;
+	        		
+	        		default:
+	        			System.out.println("Opção inválida! Tente novamente.");
+	        		
+	        	}
+	        }
+	        
+	        Pokemon oponente = new Batalha("Charmander", "Fogo", 90, 60, 25);
+	        
 	        int opcao;
 	        
 	        do {
 	            System.out.println("ArenaMon - Batalha de Pokémons");
 	            System.out.println("1 - Iniciar Batalha");
 	            System.out.println("2 - Vizualizar Pokémon");
+	            System.out.println("3 - Trocar de Pokemon");
 	            System.out.println("0 - Sair");
 	            
 	            while (!scanner.hasNextInt()) {
@@ -98,7 +195,7 @@ public class Batalha extends Pokemon {
                         int danoOponente = 0;
                         danoOponente = (10 + oponente.ataque) - jogador.defesa;
                         jogador.hp -= danoOponente;
-                        System.out.println(oponente.nome + " atacou " + jogador.nome + " causando " + danoOponente + " de dano.");
+                        System.out.println(oponente.nome + " atacou " + jogador.nome + " causando " + danoOponente + " de dano. \n");
                         
                         if (!jogador.estaVivo()) {
                             System.out.println(jogador.nome + " foi derrotado! " + oponente.nome + " venceu! \n");
@@ -112,6 +209,11 @@ public class Batalha extends Pokemon {
                     jogador.exibirPokemon();
                     
                     break;
+                    
+                case 3:
+                     jogador = ((Batalha) jogador).trocarPokemon(scanner);
+                     
+                     break;
                     
                 case 0:
                     System.out.println("Saindo do Jogo...");
